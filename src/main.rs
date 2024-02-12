836390c0360a3a4511f0bba10fc1da740e0856f0
Morgan Courbet <github@sub.ploki.fr>
2024-02-12 23:01:20 +0100

    feat: parse DOB from string

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use chrono::Month::August;
 mod eta;
 
 fn main() {
-    let dob = NaiveDate::from_ymd_opt(2024, August.number_from_month(), 20).unwrap();
+    let dob = NaiveDate::parse_from_str("2024-08-20", "%Y-%m-%d").unwrap();
     let n_of_days = eta::compute_eta(dob);
     println!("ETA: {} days", n_of_days);
 }
