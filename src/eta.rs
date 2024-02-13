6720b45b1cce9d1977b25c8e8bf803a08f680cfa
Morgan Courbet <github@sub.ploki.fr>
2024-02-13 22:37:31 +0100

    refactor: remove unnecessary explicit type

--- src/eta.rs
+++ src/eta.rs
@@ -2,7 +2,7 @@ use chrono::{Local, NaiveDate, TimeDelta};
 
 pub fn compute_eta(dob: NaiveDate) -> isize {
     let now = Local::now().date_naive();
-    let diff: TimeDelta = dob - now;
+    let diff = dob - now;
 
     diff.num_days() as isize
 }
